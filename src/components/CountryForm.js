d29c51d4bd9e90ed0e9593c2a9576172d4a83774
haerang <gofkdvjvl@naver.com>
2020-12-31 22:38:40 +0900

    feat: input field 스타일 수정

--- src/components/CountryForm.js
+++ src/components/CountryForm.js
@@ -2,19 +2,44 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import styled from 'styled-components';
 
+const Form = styled.form`
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: no-wrap;
+  padding: 20px 0;
+  & * {
+    margin: 0 10px;
+  }
+`;
+
 const InputField = styled(Field)`
   border: none;
   border-bottom: 1px solid black;
+  width: 260px;
+  padding: 7px;
+  color: gray;
+`;
+
+const Button = styled.button`
+  border-radius: 6px;
+  padding: 3px 5px;
+  border: 2px solid #b088f9;
+  // border: none;
+  color: #fff;
+  background: #98acf8;
 `;
 
 const CountryForm = ({ submit }) => (
-  <form onSubmit={submit}>
+  <Form onSubmit={submit}>
     <div>
-      <label htmlFor="search"> Search</label>
-      <InputField id="search" name="search" component="input" type="text" />
+      <label htmlFor="search">
+        <InputField id="search" name="search" component="input" type="text" />
+      </label>
     </div>
-    <button type="submit">search</button>
-  </form>
+    <Button type="submit">search</Button>
+  </Form>
 );
 
 const WrappedForm = reduxForm({
